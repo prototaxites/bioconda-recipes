94595449405b4379b57faa76d888f514142b3d16
Jim Downie <jd42@sanger.ac.uk>
2026-02-12 08:53:06 +0000

    fix(purge_dups): pass correct LDFLAGS to make to fix segfault

--- recipes/purge_dups/build.sh
+++ recipes/purge_dups/build.sh
@@ -2,11 +2,11 @@
 
 mkdir -p "${PREFIX}/bin"
 
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lz -lm"
 
 # Compile purge_dups
 cd src
-make CC="${CC}" -j"${CPU_COUNT}"
+make CC="${CC}" -j"${CPU_COUNT}" LDFLAGS="${LDFLAGS}"
 cd ../bin
 
 install -v -m 0755 calcuts get_seqs ngscstat pbcstat purge_dups split_fa "${PREFIX}/bin"
